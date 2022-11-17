be4a6394fd694cb46ebc842c49a207aa641247a6
b3nj4m1n <b3nj4m1n@gmx.net>
2022-11-17 14:49:00 +0100

    Added aoc 2021 day 3 part 2

--- 2021/day3/3.rs
+++ 2021/day3/3.rs
@@ -32,7 +32,7 @@ fn main() {
 // Struct for solution values
 struct Result {
     part_1: usize,
-    part_2: usize,
+    part_2: i64,
 }
 
 // Convert vector containing single digits of a binary number to decimal
@@ -81,4 +81,70 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         .map(|bit| if bit == &0 { 1 } else { 0 })
         .collect();
     res.part_1 = binary_to_decimal(&rate_gamma) * binary_to_decimal(&rate_epsilon);
+    let nums: Vec<i64> = inp
+        .iter()
+        .map(|num| i64::from_str_radix(num, 2).unwrap())
+        .collect();
+    // Disclaimer: this is an absolute mess
+    // Ok so here's another way for part 1
+    let mut answer_1 = 0b0;
+    for i in 0..width {
+        answer_1 = answer_1 << 1;
+        match get_most_common(&nums, (width - (i + 1))) {
+            false => answer_1 = answer_1 + 0,
+            true => answer_1 = answer_1 + 1,
+        }
+    }
+    // This basically just gets the inverse of answer_1, but because by default that would involve
+    // a bunch of leading 0's turning into 1's, we have to do some masking here
+    let black_magic = !answer_1 & (2_i32.pow(5) - 1);
+    let answer_1 = answer_1 * black_magic;
+    // And after only one year, part 2
+    let mut most_common_oxy = keep_most_common(nums.clone(), width - 1, false);
+    let mut most_common_co2 = keep_most_common(nums.clone(), width - 1, true);
+    let mut i = 1;
+    loop {
+        i = i + 1;
+        if most_common_oxy.len() != 1 {
+            most_common_oxy = keep_most_common(most_common_oxy, width - i, false);
+        }
+        if most_common_co2.len() != 1 {
+            most_common_co2 = keep_most_common(most_common_co2, width - i, true);
+        }
+        if most_common_co2.len() == 1 && most_common_oxy.len() == 1 { break; }
+    }
+    println!("{:#05b}", most_common_oxy[0]);
+    println!("{:#05b}", most_common_co2[0]);
+    res.part_2 = most_common_oxy[0] * most_common_co2[0];
+}
+
+fn keep_most_common(numbers: Vec<i64>, position: usize, invert: bool) -> Vec<i64> {
+    let most_common = get_most_common(&numbers, position) ^ invert;
+    let mask = 0b1 << position;
+    let mut result = Vec::new();
+    for number in numbers {
+        if number & mask == 0 && most_common == false {
+            result.push(number);
+        } else if number & mask != 0 && most_common == true {
+            result.push(number);
+        }
+    }
+    result
+}
+
+fn get_most_common(numbers: &Vec<i64>, position: usize) -> bool {
+    let mask = 0b1 << position;
+    let mut count_zero = 0;
+    let mut count_one = 0;
+    for number in numbers {
+        let masked = number & mask;
+        // println!("{:#07b}, {:#07b}", number, masked);
+        if masked == 0 {
+            count_zero += 1;
+        } else {
+            count_one += 1;
+        }
+    }
+    // println!("{} zeroes, {} ones.", count_zero, count_one);
+    count_zero <= count_one
 }
