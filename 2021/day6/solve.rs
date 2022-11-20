e3558a23dff237c6c189839117744ea4bd2a2719
b3nj4m1n <b3nj4m1n@gmx.net>
2022-11-20 13:14:04 +0100

    2021 6 p2

--- 2021/day6/solve.rs
+++ 2021/day6/solve.rs
@@ -32,17 +32,47 @@ fn main() {
 // Struct for solution values
 struct Result {
     part_1: usize,
-    part_2: i32,
+    part_2: usize,
 }
 
 // Function to solve both parts
 fn solve(inp: Vec<&str>, res: &mut Result) {
-    let mut fish: Vec<Fish> = inp[0].split(",").map(|t| Fish { timer: t.parse::<u8>().unwrap() }).collect();
+    let mut fish: Vec<Fish> = inp[0]
+        .split(",")
+        .map(|t| Fish {
+            timer: t.parse::<u8>().unwrap(),
+        })
+        .collect();
+    let mut fish_pool = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for f in fish.clone() {
+        fish_pool[usize::from(f.timer)] += 1
+    }
     let iterations = 80;
     for i in 0..iterations {
         fish = day(fish);
     }
     res.part_1 = fish.len();
+    let iterations_part_2 = 256;
+    /* for i in 0..(iterations_part_2 - iterations) {
+        println!("{}", i);
+        fish = day(fish);
+    }
+    res.part_2 = fish.len(); */
+    for i in 0..iterations_part_2 {
+        day_vec(&mut fish_pool);
+    }
+    /* println!("{:?}", fish_pool);
+    day_vec(&mut fish_pool); */
+    res.part_2 = fish_pool.into_iter().fold(0, |x, y| x + y);
+}
+
+fn day_vec(fish: &mut Vec<usize>) {
+    let new_fish = fish[0];
+    for i in 0..8 {
+        fish[i] = fish[i+1];
+    }
+    fish[6] += new_fish;
+    fish[8] = new_fish;
 }
 
 fn day(fish: Vec<Fish>) -> Vec<Fish> {
@@ -58,7 +88,7 @@ fn day(fish: Vec<Fish>) -> Vec<Fish> {
     result
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Fish {
-    timer: u8
+    timer: u8,
 }
