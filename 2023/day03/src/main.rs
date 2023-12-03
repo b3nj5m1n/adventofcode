766a1eaed3a384e925a74e9452cbb2580783a857
b3nj5m1n <b3nj4m1n@gmx.net>
2023-12-03 15:27:30 +0100

    2023 03 2

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::io::Read;
 
@@ -78,19 +79,27 @@ fn get_neighbors(x: usize, y: usize, max_y: usize, max_x: usize) -> Vec<(usize, 
 
 // Function to solve both parts
 fn solve(inp: Vec<&str>, res: &mut Result) {
+    let mut grid_nums = HashMap::new();
+    let mut grid_gears = HashSet::new();
     let inp: Vec<Vec<_>> = inp.iter().map(|s| s.chars().collect::<Vec<_>>()).collect();
     // dbg!(inp);
     let mut valid_nums = Vec::new();
     let mut invalid_nums = Vec::new();
+    let mut max_x = 0;
+    let max_y = inp.len();
     for (i_y, y) in inp.iter().enumerate() {
+        max_x = y.len();
         let mut on_num = false;
         let mut num = Vec::new();
         let mut num_valid = false;
         for (i_x, x) in y.iter().enumerate() {
+            if *x == '*' {
+                grid_gears.insert((i_x, i_y));
+            }
             if x.is_digit(10) {
                 on_num = true;
                 num.push(*x);
-                for neighbor in get_neighbors(i_x, i_y, inp.len(), y.len()) {
+                for neighbor in get_neighbors(i_x, i_y, max_y, max_x) {
                     if is_symbol(&inp[neighbor.1][neighbor.0]) {
                         num_valid = true;
                     }
@@ -98,12 +107,16 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
             }
             if !x.is_digit(10) || i_x == y.len() - 1 {
                 if num_valid && on_num {
-                    valid_nums.push(
-                        num.into_iter()
-                            .collect::<String>()
-                            .parse::<u32>()
-                            .expect("Couldn't parse number"),
-                    );
+                    let num_int = num
+                        .clone()
+                        .into_iter()
+                        .collect::<String>()
+                        .parse::<u32>()
+                        .expect("Couldn't parse number");
+                    valid_nums.push(num_int);
+                    for xxx in (i_x - num.len())..i_x {
+                        grid_nums.insert((xxx, i_y), num_int);
+                    }
                 } else if on_num {
                     invalid_nums.push(
                         num.into_iter()
@@ -118,7 +131,29 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
             }
         }
     }
-    /* dbg!(&valid_nums);
-    dbg!(&invalid_nums); */
     res.part_1 = valid_nums.iter().sum();
+    let mut valid_gears = Vec::new();
+    for gear in grid_gears.clone() {
+        // println!("{gear:?}");
+        let mut num_neighbors = HashSet::new();
+        for neighbor in get_neighbors(gear.0, gear.1, max_y, max_x) {
+            // println!("Considering Neighbor @ {neighbor:?}");
+            if let Some(num) = grid_nums.get(&neighbor) {
+                num_neighbors.insert(num);
+                // println!("Valid neighbor");
+            }
+        }
+        // dbg!(&num_neighbors);
+        if num_neighbors.len() == 2 {
+            let mut vals = num_neighbors.drain();
+            valid_gears.push(
+                *vals.next().expect("Unreachable") *
+                *vals.next().expect("Unreachable"),
+            );
+        }
+    }
+    // dbg!(&valid_gears);
+    // dbg!(grid_gears);
+    // dbg!(grid_nums);
+    res.part_2 = valid_gears.into_iter().sum();
 }
