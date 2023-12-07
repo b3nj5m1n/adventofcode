758f85699233a4c9afa262ce80509064e2038d12
b3nj5m1n <b3nj4m1n@gmx.net>
2023-12-07 07:15:14 +0100

    2023 07 2

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -63,7 +63,7 @@ impl TryFrom<&str> for Hand {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum HandType {
     FiveOfAKind = 7,
     FourOfAKind = 6,
@@ -74,8 +74,31 @@ enum HandType {
     HighCard = 1,
 }
 
+fn get_hand_type_norm(cards: &Vec<char>) -> anyhow::Result<HandType> {
+    let mut map: HashMap<&char, usize> = HashMap::new();
+    for c in cards.iter() {
+        map.entry(c)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+    let mut vals: Vec<usize> = map.values().map(|c| *c).collect();
+    vals.sort(); //)
+    vals.reverse();
+
+    Ok(match vals.as_slice() {
+        &[5] => HandType::FiveOfAKind,
+        &[4, 1] => HandType::FourOfAKind,
+        &[3, 2] => HandType::FullHouse,
+        &[3, 1, 1] => HandType::ThreeOfAKind,
+        &[2, 2, 1] => HandType::TwoPair,
+        &[2, 1, 1, 1] => HandType::OnePair,
+        &[1, 1, 1, 1, 1] => HandType::HighCard,
+        _ => return anyhow::bail!("Fuck"),
+    })
+}
+
 impl Hand {
-    fn get_type(&self) -> anyhow::Result<HandType> {
+    /* fn get_type(&self) -> anyhow::Result<HandType> {
         let mut map: HashMap<&char, usize> = HashMap::new();
         for c in self.cards.iter() {
             map.entry(c)
@@ -96,10 +119,68 @@ impl Hand {
             &[1, 1, 1, 1, 1] => HandType::HighCard,
             _ => return anyhow::bail!("Fuck"),
         })
+    } */
+    fn get_type(&self) -> anyhow::Result<HandType> {
+        let mut todo = vec![self.cards.clone()];
+        let mut search_space = Vec::new();
+
+        while todo.len() > 0 {
+            let current = todo.pop().unwrap();
+            if !current.contains(&'J') {
+                search_space.push(current);
+                continue;
+            }
+            if current[0] == 'J' {
+                for c in ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] {
+                    let mut new = current.clone();
+                    new[0] = c;
+                    todo.push(new);
+                }
+                continue;
+            }
+            if current[1] == 'J' {
+                for c in ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] {
+                    let mut new = current.clone();
+                    new[1] = c;
+                    todo.push(new);
+                }
+                continue;
+            }
+            if current[2] == 'J' {
+                for c in ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] {
+                    let mut new = current.clone();
+                    new[2] = c;
+                    todo.push(new);
+                }
+                continue;
+            }
+            if current[3] == 'J' {
+                for c in ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] {
+                    let mut new = current.clone();
+                    new[3] = c;
+                    todo.push(new);
+                }
+                continue;
+            }
+            if current[4] == 'J' {
+                for c in ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'] {
+                    let mut new = current.clone();
+                    new[4] = c;
+                    todo.push(new);
+                }
+                continue;
+            }
+        }
+        let mut possibilities = Vec::new();
+        for c in search_space.iter() {
+            possibilities.push(get_hand_type_norm(c).expect("Fuck"));
+        }
+        possibilities.sort();
+        Ok(*possibilities.last().unwrap())
     }
 }
 
-impl PartialOrd for Hand {
+/* impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match self.get_type().ok()?.partial_cmp(&other.get_type().ok()?) {
             Some(s) => match s {
@@ -155,6 +236,63 @@ impl PartialOrd for Hand {
         None
         // return Some(self.cards.cmp(&other.cards).reverse());
     }
+} */
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.get_type().ok()?.partial_cmp(&other.get_type().ok()?) {
+            Some(s) => match s {
+                std::cmp::Ordering::Less => {
+                    return Some(std::cmp::Ordering::Less);
+                }
+                std::cmp::Ordering::Greater => {
+                    return Some(std::cmp::Ordering::Greater);
+                }
+                std::cmp::Ordering::Equal => {}
+            },
+            None => return None,
+        }
+        // It's eq
+        for (c1, c2) in self.cards.iter().zip(other.cards.iter()) {
+            if c1 == c2 {
+                continue;
+            }
+            let c1 = match c1 {
+                'A' => 13,
+                'K' => 12,
+                'Q' => 11,
+                'T' => 10,
+                '9' => 9,
+                '8' => 8,
+                '7' => 7,
+                '6' => 6,
+                '5' => 5,
+                '4' => 4,
+                '3' => 3,
+                '2' => 2,
+                'J' => 1,
+                _ => panic!("Unreachable"),
+            };
+            let c2 = match c2 {
+                'A' => 13,
+                'K' => 12,
+                'Q' => 11,
+                'T' => 10,
+                '9' => 9,
+                '8' => 8,
+                '7' => 7,
+                '6' => 6,
+                '5' => 5,
+                '4' => 4,
+                '3' => 3,
+                '2' => 2,
+                'J' => 1,
+                _ => panic!("Unreachable"),
+            };
+            return Some(c1.cmp(&c2));
+        }
+        None
+        // return Some(self.cards.cmp(&other.cards).reverse());
+    }
 }
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
@@ -162,6 +300,7 @@ impl Ord for Hand {
     }
 }
 
+// TODO Solve part 1 again (b72fc878d13e81e41452f1657d829788838f12d7)
 // Function to solve both parts
 fn solve(inp: Vec<&str>, res: &mut Result) {
     let mut hands: Vec<Hand> = inp
@@ -176,5 +315,5 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         .collect();
     dbg!(&ranks);
     let winnings: Vec<_> = ranks.iter().map(|(i, hand)| i * hand.bid).collect();
-    res.part_1 = winnings.iter().sum();
+    res.part_2 = winnings.iter().sum();
 }
