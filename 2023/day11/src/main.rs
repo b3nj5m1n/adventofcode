b0c4f6addd0a55e1bd8cab7fbb3272fb1547f6df
b3nj5m1n <b3nj4m1n@gmx.net>
2023-12-11 08:42:06 +0100

    2023 11 2

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -36,8 +36,8 @@ fn main() {
 
 // Struct for solution values
 struct Result {
-    part_1: u32,
-    part_2: u32,
+    part_1: u128,
+    part_2: u128,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -79,13 +79,13 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         }
         xs.push(x);
     }
-    let mut count = 0;
-    for x in xs {
+    // let mut count = 0;
+    /* for x in xs {
         for row in grid.iter_mut() {
             row.insert(x + count, Tile::Space);
         }
         count += 1;
-    }
+    } */
 
     // Fill rows
     let mut ys = Vec::new();
@@ -95,69 +95,65 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         }
         ys.push(y);
     }
-    let mut count = 0;
-    for y in ys {
+    // let mut count = 0;
+    /* for y in ys {
         grid.insert(
             y + count,
             std::iter::repeat(Tile::Space).take(grid[0].len()).collect(),
         );
         count += 1;
-    }
-
-    /* // Print
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            print!("{}", grid[y][x]);
-        }
-        print!("\n");
     } */
 
-    let mut g = Graph::new();
-
-    let mut nodes = HashMap::new();
-    let mut galaxies = Vec::new();
+    // Part 1
+    let mut galaxies_cords_part_1 = Vec::new();
     for y in 0..grid.len() {
         for x in 0..grid[0].len() {
-            let a = if !nodes.contains_key(&(y,x)) {
-                let a = g.add_node((y, x));
-                nodes.insert((y, x), a);
-                a
-            } else {
-                *nodes.get(&(y,x)).unwrap()
-            };
-            for neighbour in get_neighbours((y, x), &grid) {
-                if !nodes.contains_key(&neighbour) {
-                    let b = g.add_node(neighbour);
-                    nodes.insert(neighbour, b);
-                }
-                g.add_edge(a, *nodes.get(&neighbour).unwrap(), 1);
-                g.add_edge(*nodes.get(&neighbour).unwrap(), a, 1);
-            }
             if grid[y][x] == Tile::Galaxy {
-                galaxies.push(a);
+                galaxies_cords_part_1.push((
+                    (y + ys.iter().filter(|&e| y > *e).count() * 1) as i128,
+                    (x + xs.iter().filter(|&e| x > *e).count() * 1) as i128,
+                ));
             }
         }
     }
-
-    // println!("{:?}", petgraph::dot::Dot::with_config(&g, &[petgraph::dot::Config::EdgeNoLabel]));
-
     let mut m = HashSet::new();
-    for (a, b) in galaxies.clone().into_iter().cartesian_product(galaxies) {
+    for (a, b) in galaxies_cords_part_1
+        .clone()
+        .into_iter()
+        .cartesian_product(galaxies_cords_part_1)
+    {
         if m.contains(&(a, b)) || m.contains(&(b, a)) || a == b {
             continue;
         }
-        // println!("{:?}, {:?}", g[a], g[b]);
         m.insert((a, b));
-        // dbg!(shortest_path(a, b, &grid));
-        /* let a = g.add_node(a);
-        let b = g.add_node(b); */
-        // g.add_edge(a, b, 1);
-        let path = petgraph::algo::astar(&g, a, |finish| finish == b, |e| *e.weight(), |_| 0).unwrap();
-        // dbg!(&path);
-        // assert!(path.is_some());
-        res.part_1 += path.0;
+        res.part_1 += ((b.0 - a.0).abs() + (b.1 - a.1).abs()) as u128;
     }
-    // dbg!(galaxies);
+
+    // Part 2
+    let factor = 1000000;
+    let mut galaxies_cords_part_2 = Vec::new();
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == Tile::Galaxy {
+                galaxies_cords_part_2.push((
+                    (y + ys.iter().filter(|&e| y > *e).count() * ( factor - 1 )) as i128,
+                    (x + xs.iter().filter(|&e| x > *e).count() * ( factor - 1 )) as i128,
+                ));
+            }
+        }
+    }
+    let mut m = HashSet::new();
+    for (a, b) in galaxies_cords_part_2
+        .clone()
+        .into_iter()
+        .cartesian_product(galaxies_cords_part_2)
+    {
+        if m.contains(&(a, b)) || m.contains(&(b, a)) || a == b {
+            continue;
+        }
+        m.insert((a, b));
+        res.part_2 += ((b.0 - a.0).abs() + (b.1 - a.1).abs()) as u128;
+    }
 }
 
 fn get_neighbours(tile: (usize, usize), grid: &Vec<Vec<Tile>>) -> Vec<(usize, usize)> {
@@ -177,55 +173,11 @@ fn get_neighbours(tile: (usize, usize), grid: &Vec<Vec<Tile>>) -> Vec<(usize, us
     result
 }
 
-/* fn shortest_path(start: (usize, usize), end: (usize, usize), grid: &Vec<Vec<Tile>>) -> Option<u32> {
-    println!("Finding shortest path from {start:?} to {end:?}");
-    if start == end {
-        return Some(0);
-    }
-    let neighbours = get_neighbours(start, grid)
-        .into_iter()
-        .map(|neighbour| {
-            (
-                f64::sqrt(
-                    ((end.0 as f64 - neighbour.0 as f64) * (end.0 as f64 - neighbour.0 as f64)
-                        + (end.1 as f64 - neighbour.1 as f64)
-                            * (end.1 as f64 - neighbour.1 as f64)),
-                ),
-                neighbour,
-            )
-        })
-        .sorted_by(|a, b| a.0.total_cmp(&b.0));
-    let min = neighbours
-        .clone()
-        .min_by(|a, b| a.0.total_cmp(&b.0))
-        .map(|n| n.0);
-    let to_consider = neighbours.filter(|n| Some(n.0) == min);
-    to_consider.into_iter().map(|current| shortest_path(current.1, end, grid)).min().flatten()
-}
-
-/* let mut steps = 0;
-let mut current = start;
-loop {
-    let neighbours: Vec<_> = get_neighbours(current, grid)
-        .into_iter()
-        .map(|neighbour| {
-            (
-                f64::sqrt(
-                    ((end.0 as f64 - neighbour.0 as f64) * (end.0 as f64 - neighbour.0 as f64)
-                        + (end.1 as f64 - neighbour.1 as f64)
-                            * (end.1 as f64 - neighbour.1 as f64)),
-                ),
-                neighbour,
-            )
-        })
-        .sorted_by(|a, b| a.0.total_cmp(&b.0))
-        .collect();
-    let shortest_neighbor = neighbours[0].1;
-    steps += 1;
-    if shortest_neighbor == end {
-        break;
-    }
-    current = shortest_neighbor;
-    dbg!(current);
-}
-return steps; */ */
+// As usual made things harder for myself than necessary, for part 1 I actually created the whole
+// graph using petgraph, then ran astar over all the galaxy nodes to find the solution. (about 3
+// minutes of runtime)
+// Struggled way too hard with this until I finally realised how easily you can compute the
+// distance between two points and at that point it was pretty obvious you didn't actually have to
+// insert any rows/columns for empty space.
+// Took way longer than I would have liked but at least I didn't have to look up any solution like
+// yesterday.
