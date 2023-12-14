fdcce287723649df6ce12e41f325e8ffe34a305b
b3nj5m1n <b3nj4m1n@gmx.net>
2023-12-14 07:33:42 +0100

    2023 14 2

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::io::Read;
 
@@ -37,33 +38,94 @@ struct Result {
 
 // Function to solve both parts
 fn solve(inp: Vec<&str>, res: &mut Result) {
-    /* for (i, line) in inp.iter().enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            if c == 'O' {
-                for (ii, line) in grid.iter_mut().enumerate().rev() {
-                    if ii < i {
-                        if grid.clone()[ii-1].chars.nth(j) == ''
-                        grid[i].chars().nth(j);
-                    }
-                }
-            }
+    let north = transpose(inp.into_iter().map(|l| l.to_string()).collect(), 1);
+    let north = tilt_north(north);
+    let back = transpose(north.clone(), 3);
+    // dbg!(&transpose(north.clone(), 3));
+    res.part_1 = count(back);
+    let west = transpose(north, 3);
+    let west = tilt_north(west);
+    // dbg!(&transpose(west.clone(), 0));
+    let south = transpose(west.clone(), 3);
+    let south = tilt_north(south);
+    // dbg!(&transpose(south.clone(), 1));
+    let east = transpose(south, 3);
+    let east = tilt_north(east);
+    // dbg!(&transpose(east.clone(), 2));
+    let mut back = transpose(east.clone(), 2);
+
+    // Find a cycle in the NWSE shifts, once a cycle has been found we can skip most of the
+    // iterations and jump straight to the last few
+    let mut map = HashMap::new();
+    let bound = 1000000000;
+    map.insert(back.clone(), bound);
+    let mut i = bound - 1;
+    while i > 0 {
+        back = cycle(back);
+        if map.contains_key(&back.clone()) {
+            let c_len = map.get(&back.clone()).unwrap() - i;
+            i = (i - 1) % (c_len);
+            break;
         }
-        // println!("{}", line)
-    } */
-    let transposed: Vec<String> = (0..inp[0].len())
+        map.insert(back.clone(), i);
+        i -= 1;
+    }
+    for _ in 0..i {
+        back = cycle(back);
+    }
+
+    res.part_2 = count(back);
+}
+
+fn cycle(back: Vec<String>) -> Vec<String> {
+    let north = transpose(back, 1);
+    let north = tilt_north(north);
+    let west = transpose(north, 3);
+    let west = tilt_north(west);
+    let south = transpose(west.clone(), 3);
+    let south = tilt_north(south);
+    let east = transpose(south, 3);
+    let east = tilt_north(east);
+    transpose(east.clone(), 2)
+}
+
+fn count(inp: Vec<String>) -> usize {
+    inp.iter()
         .rev()
-        .map(|col| {
-            (0..inp.len())
-                .map(|row| inp[row].chars().nth(col).expect("fuck").clone())
-                .collect()
-        })
-        .collect();
+        .enumerate()
+        .map(|(i, l)| l.to_string().chars().filter(|c| c == &'O').count() * (i + 1))
+        .sum()
+}
+
+fn transpose(inp: Vec<String>, count: u8) -> Vec<String> {
+    let mut transposed: Vec<String> = inp.clone().into_iter().map(|l| l.to_string()).collect();
+    for _ in 0..count {
+        transposed = (0..transposed.clone()[0].to_string().len())
+            .rev()
+            .map(|col| {
+                (0..transposed.len())
+                    .map(|row| {
+                        transposed[row]
+                            .to_string()
+                            .chars()
+                            .nth(col)
+                            .expect("fuck")
+                            .clone()
+                    })
+                    .collect::<String>()
+            })
+            .collect();
+    }
+    transposed
+    // transposed.into_iter().map(|l| l.to_string()).collect::<Vec<String>>()
+}
+
+fn tilt_north(inp: Vec<String>) -> Vec<String> {
     let mut grid = Vec::new();
-    dbg!(&transposed);
-    for line in transposed {
+    for line in inp {
         let mut new_line = String::new();
         let mut skip = false;
-        for (i, c) in line.chars().enumerate() {
+        for (i, c) in line.to_string().chars().enumerate() {
             if c == '#' {
                 new_line.push_str("#");
                 skip = false;
@@ -71,12 +133,14 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
                 continue;
             } else {
                 let s = &line
+                    .to_string()
                     .chars()
                     .skip(i)
                     .filter(|c| c != &'.')
                     .take_while(|c| c != &'#')
                     .collect::<String>();
                 let dots = &line
+                    .to_string()
                     .chars()
                     .skip(i)
                     .take_while(|c| c != &'#')
@@ -89,20 +153,5 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         }
         grid.push(new_line);
     }
-    let transposed_back: Vec<String> = (0..grid[0].len())
-        .map(|col| {
-            (0..grid.len())
-                .rev()
-                .map(|row| grid[row].chars().nth(col).expect("fuck").clone())
-                .collect()
-        })
-        .collect();
-    let r: usize = transposed_back
-        .iter()
-        .rev()
-        .enumerate()
-        .map(|(i, l)| l.chars().filter(|c| c == &'O').count() * ( i+1 ))
-        .sum();
-    dbg!(&transposed_back);
-    dbg!(r);
+    grid
 }
