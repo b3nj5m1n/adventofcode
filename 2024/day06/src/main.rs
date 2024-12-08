2a31f599a3a7c935cebe069e0dbd48034a1406db
b3nj5m1n <b3nj4m1n@gmx.net>
2024-12-08 18:10:56 +0100

    2024 06 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 use std::io::Read;
 use std::{env, thread, time};
@@ -36,7 +37,7 @@ struct Result {
     part_2: usize,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Obstacle,
@@ -61,7 +62,7 @@ impl Display for Tile {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Guard {
     Up,
     Down,
@@ -76,6 +77,75 @@ fn print_grid(grid: &Vec<Vec<Tile>>) {
         }
         print!("\n");
     }
+}
+
+fn run_simulation(
+    guard_pos: Option<(usize, usize)>,
+    grid: &Vec<Vec<Tile>>,
+) -> Option<Vec<(usize, usize)>> {
+    let mut guard_pos = guard_pos;
+    let mut grid = grid.clone();
+    let mut loop_points = HashSet::new();
+    while let Some(guard_pos_) = guard_pos {
+        let id = (guard_pos_, grid[guard_pos_.1][guard_pos_.0]);
+        if loop_points.contains(&id) {
+            return None;
+        }
+        loop_points.insert(id);
+        // dbg!(guard_pos_);
+        // print_grid(&grid);
+        let target_pos = match grid[guard_pos_.1][guard_pos_.0] {
+            Tile::Guard(g) => match g {
+                Guard::Up => (guard_pos_.0 as i64, guard_pos_.1 as i64 - 1),
+                Guard::Down => (guard_pos_.0 as i64, guard_pos_.1 as i64 + 1),
+                Guard::Right => (guard_pos_.0 as i64 + 1, guard_pos_.1 as i64),
+                Guard::Left => (guard_pos_.0 as i64 - 1, guard_pos_.1 as i64),
+            },
+            _ => unreachable!(),
+        };
+        if target_pos.0 < 0
+            || target_pos.1 < 0
+            || target_pos.0 >= grid[0].len() as i64
+            || target_pos.1 >= grid.len() as i64
+        {
+            grid[guard_pos_.1][guard_pos_.0] = Tile::Visited;
+            guard_pos = None;
+        } else if grid[target_pos.1 as usize][target_pos.0 as usize] == Tile::Obstacle {
+            match grid[guard_pos_.1][guard_pos_.0] {
+                Tile::Guard(g) => match g {
+                    Guard::Up => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Right),
+                    Guard::Down => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Left),
+                    Guard::Right => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Down),
+                    Guard::Left => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Up),
+                },
+                _ => unreachable!(),
+            };
+        } else {
+            // println!("Setting new guard pos");
+            guard_pos = Some((target_pos.0 as usize, target_pos.1 as usize));
+            grid[target_pos.1 as usize][target_pos.0 as usize] = grid[guard_pos_.1][guard_pos_.0];
+            grid[guard_pos_.1][guard_pos_.0] = Tile::Visited;
+        }
+        // thread::sleep(time::Duration::from_secs(1));
+    }
+    Some(
+        grid.iter()
+            .enumerate()
+            .map(|(y, r)| {
+                r.iter()
+                    .enumerate()
+                    .filter_map(|(x, t)| {
+                        if let Tile::Visited = t {
+                            Some((x, y))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .flatten()
+            .collect::<Vec<_>>(),
+    )
 }
 
 // Function to solve both parts
@@ -117,50 +187,27 @@ fn solve(inp: Vec<&str>, res: &mut Result) {
         })
         .filter(|n| n.len() != 0)
         .collect::<Vec<_>>()[0][0];
-    let mut guard_pos = Some(*initial_guard_pos);
-    while let Some(guard_pos_) = guard_pos {
-        // dbg!(guard_pos_);
-        // print_grid(&grid);
-        let target_pos = match grid[guard_pos_.1][guard_pos_.0] {
-            Tile::Guard(g) => match g {
-                Guard::Up => (guard_pos_.0 as i64, guard_pos_.1 as i64 - 1),
-                Guard::Down => (guard_pos_.0 as i64, guard_pos_.1 as i64 + 1),
-                Guard::Right => (guard_pos_.0 as i64 + 1, guard_pos_.1 as i64),
-                Guard::Left => (guard_pos_.0 as i64 - 1, guard_pos_.1 as i64),
-            },
-            _ => unreachable!(),
-        };
-        if target_pos.0 < 0
-            || target_pos.1 < 0
-            || target_pos.0 >= grid[0].len() as i64
-            || target_pos.1 >= grid.len() as i64
-        {
-            grid[guard_pos_.1][guard_pos_.0] = Tile::Visited;
-            guard_pos = None;
-        } else if grid[target_pos.1 as usize][target_pos.0 as usize] == Tile::Obstacle {
-            match grid[guard_pos_.1][guard_pos_.0] {
-                Tile::Guard(g) => match g {
-                    Guard::Up => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Right),
-                    Guard::Down => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Left),
-                    Guard::Right => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Down),
-                    Guard::Left => grid[guard_pos_.1][guard_pos_.0] = Tile::Guard(Guard::Up),
-                },
-                _ => unreachable!(),
-            };
-        } else {
-            // println!("Setting new guard pos");
-            guard_pos = Some((target_pos.0 as usize, target_pos.1 as usize));
-            grid[target_pos.1 as usize][target_pos.0 as usize] = grid[guard_pos_.1][guard_pos_.0];
-            grid[guard_pos_.1][guard_pos_.0] = Tile::Visited;
-        }
-        // thread::sleep(time::Duration::from_secs(1));
-    }
-    res.part_1 = grid
+    let guard_pos = Some(*initial_guard_pos);
+
+    let visited_coords_original_config = run_simulation(guard_pos, &grid).expect("");
+
+    res.part_1 = visited_coords_original_config.len();
+
+    let visited_coords_original_config: Vec<_> = visited_coords_original_config
+        .into_iter()
+        .filter(|(x, y)| !(*x == initial_guard_pos.0 && *y == initial_guard_pos.1))
+        .collect();
+    res.part_2 = visited_coords_original_config
         .iter()
-        .map(|r| {
-            r.iter()
-                .map(|t| if let Tile::Visited = t { 1 } else { 0 })
-                .sum::<usize>()
+        .map(|(x, y)| {
+            println!("{}/{}", y, grid.len());
+            let mut new_grid = grid.clone();
+            new_grid[*y][*x] = Tile::Obstacle;
+            if let None = run_simulation(guard_pos, &new_grid) {
+                1
+            } else {
+                0
+            }
         })
-        .sum::<usize>();
+        .sum();
 }
